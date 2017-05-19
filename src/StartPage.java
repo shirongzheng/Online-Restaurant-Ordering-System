ab73f4e53a78cc6b24542d86e6d07e4d05abb787
Shirong Zheng <shirongzheng1027@gmail.com>
2017-05-18 21:53:43 -0400

    Update StartPage.java

--- src/StartPage.java
+++ src/StartPage.java
@@ -1,7 +1,3 @@
-/* CSC32200-E  Online Restaurant Ordering System Project
-   Shirong Zheng      Yuhao Liu     Qiuhan Piao
- */
-
 import java.awt.Color;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
